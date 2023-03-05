9a4130a1f769721eb26e5674eb9884bb01761cad
Fedlimit <34999050+Fedlimit@users.noreply.github.com>
2023-03-05 23:32:33 +0800

    Delete pdfexpert.js

--- pdfexpert.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body);
-
-function key() {
-    obj[arguments[0]] = arguments[1]
-    }
-
-key("inAppStates", '[{"productId":"com.readdle.PDFExpert5.subscription.year50_pe6","subscriptionExpirationIntent":"userCancelled","receiptStatus":"ok","subscriptionExpirationDate":"13:15 03/11/2099","isEligibleForIntroPeriod":false,"originalTransactionId":"20000618444996","productName":"subscription","isInBillingRetryPeriod":false,"type":"subscription","subscriptionState":"active","subscriptionAutoRenewStatus":"autoRenewOff","isInGracePeriod":false}]');
-
-$done({body: JSON.stringify(obj)});
